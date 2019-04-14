36fc24f0500cbcc86bd78c0ff241be51e9389278
Christopher Murray <cmurray21@student.clayton.edu>
2019-04-14 18:00:08 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+
+function getOrder() {
+
+    alert("Thanks for your order!");
+
+    alert("Really, we mean it.");
+
+    alert("It's customer's like you that keep us in business");
+
+    alert("It can be tough sometimes. We have had to get a second mortgage just to keep the Spaghetti O's on the table.");
+
+    alert("Did you want anything else?");
+  }
